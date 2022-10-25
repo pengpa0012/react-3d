ceaf3ac69922f004c6cce1141caeeff519a1d5dd
Pengpa0012 <peng.ilikha@gmail.com>
2022-10-25 19:33:30 +0800

    added framer motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
+import { Canvas } from '@react-three/fiber'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 
 const Home: NextPage = () => {
-
+  const [width, setWidth] = useState<number>(0)
   return (
     <div>
-      <h1>Hello world</h1>
+      <Canvas>
+        <ambientLight intensity={.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry args={[width, 2, 2]} />
+        </mesh>
+      </Canvas>
+      <h1 onMouseOver={() => setWidth(width + 1)}>HOVER</h1>
     </div>
   )
 }
